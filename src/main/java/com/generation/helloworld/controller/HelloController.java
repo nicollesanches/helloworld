1e6495f72ed7fea1a85fa753610b888710fb2ac7
Nicolle Sanches <nicollesanches.ri@outlook.com>
2023-04-12 23:22:53 -0300

    atv.spring

--- src/main/java/com/generation/helloworld/controller/HelloController.java
+++ src/main/java/com/generation/helloworld/controller/HelloController.java
@@ -17,5 +17,25 @@ public class HelloController {
 	@GetMapping ("/nome")
 	public String nome() {
 		return "<b>Nicolle<b>";
+		
 }
+	@GetMapping ("/bsms")
+	public String bsms() {
+		return "<h1 style = \"color: Blue\"> \r\n"
+				+ "Essas são as BSM'S\r\n"
+				+ "\r\n"
+				+ "</h1>\r\n"
+				+ "\r\n"
+				+ "<h3 style = \"color: red\">\r\n"
+				+ "Mentalidade de crescimento<br>\r\n"
+				+ "Persistência<br>\r\n"
+				+ "Responsabilidade pessoal<br>\r\n"
+				+ "Orientação ao futuro<br>\r\n"
+				+ "Comunicação<br>\r\n"
+				+ "Orientação ao detalhe<br>\r\n"
+				+ "Proatividade<br>\r\n"
+				+ "Trabalho em equipe<br>\r\n";
+				
+}
+
 }
